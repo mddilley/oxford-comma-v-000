3461311e16a2005d6cf30be5946b03bfab22d23e
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 16:45:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  new_array = []
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    last_item = array.last
+    array.pop
+    return array.join(", ") + ", and " + last_item.to_s
+  end
 end
