c8e9615df02ffe5a5ffecbe04ebfad9fbc61fabe
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 16:42:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  new_array = []
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    new_array = array
+    new_array.pop
+    return new_array.join(", ") + ", and " + array.last.to_s
+  end
 end
